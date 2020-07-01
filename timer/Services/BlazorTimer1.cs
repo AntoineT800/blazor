3ae0faa17393de53964b924fbbb75fed8b167c30
thumerelle <antoine.thumerelle@orange.fr>
2020-07-01 21:01:38 +0200

    alternance

---
+++ timer/Services/BlazorTimer1.cs
@@ -0,0 +1,28 @@
+﻿namespace Services
+{
+    using System;
+    using System.Timers;
+    internal class BlazorTimer
+    {
+        private Timer _timer;
+
+        public void SetTimer(double interval)
+        {
+            _timer = new Timer(interval);
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        public event Action OnElapsed;
+
+        public void StopTimer()
+        {
+            _timer.Stop();
+        }
+        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+        }
+    }
+}
