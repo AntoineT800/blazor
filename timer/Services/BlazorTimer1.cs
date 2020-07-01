055afdd23e07a710c20bf9819d8dcf3f664841db
thumerelle <antoine.thumerelle@orange.fr>
2020-07-01 21:21:50 +0200

    alternance

--- timer/Services/BlazorTimer1.cs
+++ timer/Services/BlazorTimer1.cs
@@ -8,19 +8,20 @@
 
         public void SetTimer(double interval)
         {
-            _timer = new Timer(interval);
+            _timer = new Timer(interval); // Permet d'initialiser un timer 
             _timer.Elapsed += NotifyTimerElapsed;
             _timer.AutoReset = true;
-            _timer.Enabled = true;
+            _timer.Enabled = true;      
         }
 
-        public event Action OnElapsed;
+        public event Action OnElapsed; //Permet d'arrêter le Timer
 
         public void StopTimer()
         {
-            _timer.Stop();
+            _timer.Stop(); //Quand l'intervalle rentré en paramètre dans la fonction SetTimer() est terminé déclenche l'event OnElapsed  public event Action OnElapsed;
+
         }
-        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+    private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
         {
             OnElapsed?.Invoke();
         }
